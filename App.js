0d187718ed84ea0a35691dded41c88ac127e86b4
Haudihou <newasoft@hotmail.com>
2023-08-30 12:22:42 +0300

    heartrates

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState(0);
+  const [hrlimits, setHrLimits] = useState('');
+
+  const calculate = () => {
+    const calculatedAge = isNaN(age) ? 0 : age;
+    const lower = (220 - calculatedAge) * 0.65;
+    const upper = (220 - calculatedAge) * 0.85;
+    setHrLimits(`${lower.toFixed(0)} - ${upper.toFixed(0)}`);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.label}>Age</Text>
+      <TextInput
+        placeholder='Enter your age'
+        value={age === 0 ? '' : age.toString()}  // Handle initial value correctly
+        onChangeText={inputAge => setAge(inputAge)}
+        keyboardType='numeric'  // Add keyboardType to restrict input to numbers
+        style={styles.field}
+      />
+      <Text style={styles.label}>Hr Limits</Text>
+      <Text style={styles.field}>{hrlimits}</Text>
+      <Button title='Calculate' onPress={calculate} />
     </View>
   );
 }
@@ -14,7 +33,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  field: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 8,
   },
 });
